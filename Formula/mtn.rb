a0a0e2ec7c06a51fb11f0f406d447763aa6e57e8
Raoul Schaffner <raoul.schaffner@eawag.ch>
2024-04-21 12:55:45 +0200

    Update mtn.rb

--- Formula/mtn.rb
+++ Formula/mtn.rb
@@ -8,13 +8,7 @@ class Mtn < Formula
   depends_on "gd"
 
   def install
-    system "make", "GB_F_FONTNAME=Supplemental/Tahoma Bold.ttf", "-Csrc", "install", "PREFIX=#{prefix}"
+    system "make", "-Csrc", "install", "PREFIX=#{prefix}"
   end
 
-  test do
-    system "curl", "--output", "sample.avi", "-L", "https://bitbucket.org/wahibre/mtn/downloads/sample.avi"
-    system "echo '918243383bc9a3a8ff37da451f4b6b17f9636769  sample_s.jpg' > checksum"
-    system "#{bin}/mtn", "sample.avi"
-    system "shasum", "--check", "checksum"
-  end
 end
